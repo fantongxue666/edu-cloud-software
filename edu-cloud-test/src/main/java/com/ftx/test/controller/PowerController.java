7ba481b685334586a5b736c91aedda9764edf5df
fanjiangfeng <fantongxue666@163.com>
2021-02-04 20:57:44 +0800

    分布式配置服务

--- edu-cloud-test/src/main/java/com/ftx/test/controller/PowerController.java
+++ edu-cloud-test/src/main/java/com/ftx/test/controller/PowerController.java
@@ -1,6 +1,7 @@
 package com.ftx.test.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +15,12 @@ public class PowerController {
     public String test(){
         String string = testInterface.getString();
         return string;
+    }
+
+    @Value("${user.name}")
+    String foo;
+    @RequestMapping(value = "/hi")
+    public String hi(){
+        return foo;
     }
 }
