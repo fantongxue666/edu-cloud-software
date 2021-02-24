ba31797d2acfe1d886ee575b9c75ecaf6445ba9f
fanjiangfeng <fantongxue666@163.com>
2021-02-24 18:45:44 +0800

    2021/2/24

--- edu-cloud-test/src/main/java/com/ftx/test/TestApplication.java
+++ edu-cloud-test/src/main/java/com/ftx/test/TestApplication.java
@@ -6,12 +6,14 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableDiscoveryClient
 @EnableFeignClients
+@ComponentScan(basePackages = {"com.ftx.authentication.rainshiro.**","com.rain.platentity.**"})
 public class TestApplication {
 
     public static void main(String[] args) {
